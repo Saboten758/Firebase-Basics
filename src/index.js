2f7607855140eaeb878fb4d2f335da9707debd1a
Debjit Daw <debjitdaw03@gmail.com>
2022-12-07 22:45:44 +0530

    added user authentication and subscription to all users in database

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import{ initializeApp } from 'firebase/app'
+
 import {
     getFirestore, collection, onSnapshot,
     addDoc, deleteDoc, doc, 
     query,where,
     orderBy, serverTimestamp,
-    getDoc
+    getDoc,updateDoc,getDocs
 } from 'firebase/firestore'
+
+import{
+    getAuth, createUserWithEmailAndPassword,
+    signOut,signInWithEmailAndPassword,
+    onAuthStateChanged
+}from 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyAZw_7AKuS8ikrOsmlSAdJJxowddWSotns",
@@ -20,6 +27,7 @@ const firebaseConfig = {
 initializeApp(firebaseConfig) //starts the firebase config in the web app
 
 const db=getFirestore()  //for getting the entire firebase database
+const auth=getAuth()
 
 const colRef= collection(db,'books') //refernce to a collection in the database called 'books'
 
@@ -51,6 +59,10 @@ onSnapshot(q,(snapshot)=>{  //taking a snashot (state of an event) of the entire
         .then(()=>{
             addBookForm.reset()  //as it is async func., we can perform then function after it has been completed
         })
+        .catch((err)=>{  //for catching any error
+            console.log(err.message)
+            alert("Error!")
+        })
     })
 
     const deleteBookForm = document.querySelector('.delete') //for deleting the document
@@ -60,6 +72,10 @@ onSnapshot(q,(snapshot)=>{  //taking a snashot (state of an event) of the entire
         deleteDoc(docRef)
             .then(()=>{
                 deleteBookForm.reset()
+            })
+            .catch((err)=>{  //for catching any error
+                console.log(err.message)
+                alert("No Such file Found!")
             })
     })
 
@@ -73,3 +89,116 @@ onSnapshot(q,(snapshot)=>{  //taking a snashot (state of an event) of the entire
 onSnapshot(docRef, (doc) => {
     console.log(doc.data(),doc.id)
 }) //subscribes whenever a document is changed(documnet properties are changed)
+
+const updateForm= document.querySelector('.update')
+updateForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const docRef=doc(db,'books',updateForm.id.value)
+    updateDoc(docRef,{     //takes a doc referrence and an object which may or may not have all the properties of the original collection
+        title: updateForm.new.value
+    })
+    .then(()=>{
+        updateForm.reset()
+    })
+    .catch((err)=>{  //for catching any error
+        console.log(err.message)
+        alert("No Such file Found!")
+    })
+})
+
+const li= document.querySelector('.tlist')
+li.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    getDocs(colRef)
+    .then((snapshot)=>{
+        let books=[]
+        snapshot.docs.forEach((doc)=>{
+            books.push({...doc.data() ,id: doc.id})
+        })
+        for(var i=0;i<books.length;i++)
+        {
+            alert(books[i].title)
+        }
+    })
+})
+const lim= document.querySelector('.alist')
+lim.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    getDocs(colRef)
+    .then((snapshot)=>{
+        let books=[]
+        snapshot.docs.forEach((doc)=>{
+            books.push({...doc.data(),id: doc.id})
+        })
+        for(var i=0;i<books.length;i++)
+        {
+            alert(books[i].author)
+        }
+    })
+})
+const lm = document.querySelector('.ilist')
+lm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    getDocs(colRef)
+    .then((snapshot)=>{
+        let books=[]
+        snapshot.docs.forEach((doc)=>{
+            books.push({...doc.data(),id: doc.id})
+        })
+        for(var i=0;i<books.length;i++)
+        {
+            alert(books[i].id)
+        }
+    })
+})
+
+const signin=document.querySelector('.signin')  //for new user
+signin.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const email=signin.email.value
+    const pass=signin.password.value
+
+    createUserWithEmailAndPassword(auth,email,pass)
+    .then((cred)=>{
+        alert("User Created:",cred.user)
+        //console.log(cred.user)
+        signin.reset()
+    })
+    .catch((err)=>{
+        alert(err.message)
+    })
+})
+
+const login=document.querySelector('.login')  //for existing login
+login.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    const email=login.email.value
+    const pass=login.password.value
+
+    signInWithEmailAndPassword(auth,email,pass)
+    .then((cred)=>{
+        //console.log("user logged in:",cred.user)
+        alert("Logged in:",cred.user)
+    })
+    .catch((err)=>{
+        console.log(err.message)
+        alert('Something went wrong! Check Logs')
+    })
+})
+
+const logout=document.querySelector('.logout') //for logout
+logout.addEventListener('click',()=>{
+    signOut(auth)
+    .then(()=>{
+        alert("Signed out Successfully!")
+    })
+    .catch((err)=>{
+        console.log(err.message)
+        alert('Something went wrong! Check Logs')
+    })
+})
+
+onAuthStateChanged(auth,(user)=>{
+    console.log("User Status Changed:",user)
+})
